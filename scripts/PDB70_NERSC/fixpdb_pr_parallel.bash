d88c6c3e6cfd72291f9709236471f65d9432b8e6
Feng Yu <fyu2@lbl.gov>
2024-03-01 14:57:04 -0800

    replace the old bash script with new script to fix the conformations.

--- scripts/PDB70_NERSC/fixpdb_pr_parallel.bash
+++ scripts/PDB70_NERSC/fixpdb_pr_parallel.bash
@@ -39,6 +39,7 @@ fi
 pdb_loc=${outdir}/pdb
 seq_loc=${outdir}/sequence
 pr_loc=${outdir}/saxs_r
+ref_seq_loc=${outdir}/ref_seq
 
 #mkdir -p ${pdb_loc}
 #echo "created PDB storage folder at ${pdb_loc}"
@@ -48,14 +49,22 @@ pr_loc=${outdir}/saxs_r
 #echo "created saxs P(r) storage folder at ${pr_loc}"
 
 if [ -f "$file" ]; then
-       done=`grep -c ${file} complete.txt`
-       if [ "$done" -ne "0" ] ; then
-             return
-       fi
+   done=`grep -c ${file} complete.txt`
+   echo $done
+   if [ "$done" -ne "0" ] ; then
+       exit
+   fi
    echo ${file}
+   
    filename=$(basename "$file")
+   prefix="${filename%.*}"
+   IFS='_' read -r PDB_code chain_ID <<< "$prefix"
    echo ${filename}
-   pdbfixer ${file} --output "${pdb_loc}/fixed_${filename}" --keep-heterogens=none --replace-nonstandard --add-residues --verbose
+   
+   python get_full_seq.py -f ${file} -o ${ref_seq_loc}
+   pdbfixer ${file} --output "${pdb_loc}/fixed_${filename}" \
+   --keep-heterogens=none --replace-nonstandard --add-residues --add-atoms=all  --verbose \
+   --sequence "${ref_seq_loc}/fixed_${prefix}.fasta" --chain_id "${chain_ID}"
    calc-pr "${pdb_loc}/fixed_${filename}" -o "${pr_loc}/${filename}.pr.csv"
    python PDBtoSeq.py  -f "${pdb_loc}/fixed_${filename}" -o "${seq_loc}/${filename}.fasta" 
    echo ${file} >> complete.txt
