0972d24d591fd916fb14f46555666abe8b16f393
Steph Prince <40640337+stephprince@users.noreply.github.com>
2024-09-10 13:59:34 -0700

    add utils for structure comparison

--- src/metfish/utils.py
+++ src/metfish/utils.py
@@ -1,6 +1,7 @@
 import os
 import warnings
 import numpy as np
+import pandas as pd
 
 from Bio.PDB.MMCIFParser import FastMMCIFParser
 from Bio.PDB.PDBParser import PDBParser
@@ -9,9 +10,17 @@ from pathlib import Path
 from periodictable import elements
 from scipy.spatial.distance import pdist, squareform
 
+from Bio.PDB.MMCIFParser import FastMMCIFParser
+from Bio.PDB.PDBParser import PDBParser
+from Bio.SVDSuperimposer import SVDSuperimposer
+from Bio import SeqUtils, Align
+from Bio.PDB import PDBIO
+
+from periodictable import elements
+from scipy.spatial.distance import pdist, squareform
 
 n_elec_df = {el.symbol: el.number for el in elements}
-
+amino_acids = [a.upper() for a in SeqUtils.IUPACData.protein_letters_3to1.keys()]
 
 def get_Pr(structure, structure_id="", dmax=None, step=0.5):
     """
@@ -94,3 +103,188 @@ def extract_seq(pdb_input, output_path):
     seq = "\n".join([f">{pdb_name}", seq])
     with open(output_path, "w") as f:
         f.write(seq)
+
+def get_single_letter_sequences(residues):
+    ret = list()
+    for res in residues:
+        res = res[0] + res[1:].lower()
+        ret.append(SeqUtils.IUPACData.protein_letters_3to1[res])
+    return "".join(ret)
+
+def align_sequences(ref_df, query_df):
+    """ Align protein sequences """
+    ref_seq = get_single_letter_sequences(ref_df['residue_name'])
+    query_seq = get_single_letter_sequences(query_df['residue_name'])
+    
+    # if not the same sequence, align
+    if ref_seq != query_seq:
+        aligner = Align.PairwiseAligner()
+        alignments = aligner.align(ref_seq, query_seq)
+
+        ref_idx, query_idx = alignments[0].indices[:, ~(alignments[0].indices == -1).any(axis=0)]
+        
+        ref_df = ref_df.iloc[ref_idx]
+        query_df = query_df.iloc[query_idx]
+
+    return ref_df, query_df
+
+def clean_pdb_df(pdb_df, atom_types):
+    alt_locs = pdb_df['alt_loc'].unique()
+    alt_locs = alt_locs[:2] if len(alt_locs) > 1 else alt_locs
+
+    pdb_df = pdb_df.query(f"residue_name in {amino_acids} & atom_name in {atom_types}")
+    pdb_df = pdb_df[pdb_df["alt_loc"].isin(alt_locs)]  # query not work well with empty strings'
+
+    return pdb_df
+
+def superimpose_structures(fixed_df, moving_df, atom_types=["CA", "N", "C", "O"]):
+    """ Superimpose two protein structures. 
+
+    Args:
+        fname_fixed (str): path to PDB file
+        fname_moving (str): path to PDB file
+        atom_types (list): atom types to align structures with, traditionally aligned with either
+        1) only alpha-carbon atoms (CA), or 2) the "protein backbone" atoms (CA, N, C, O), or all atoms
+
+    Returns:
+        superimposer: returns instance of BioPython SVDSuperImposer class
+    """
+
+    # filter for atom types and amino acide residues only
+    fixed_atom_df = clean_pdb_df(fixed_df, atom_types)
+    moving_atom_df = clean_pdb_df(moving_df, atom_types)
+
+    # align sequences (if already aligned, will return same df)
+    fixed_atom_df, moving_atom_df = align_sequences(fixed_atom_df, moving_atom_df)
+
+    # get coordinates of the atoms
+    fixed_coords = fixed_atom_df[['x_coord', 'y_coord', 'z_coord']].to_numpy()
+    moving_coords = moving_atom_df[['x_coord', 'y_coord', 'z_coord']].to_numpy()
+
+    # superimpose structures
+    si = SVDSuperimposer()
+    si.set(fixed_coords, moving_coords)
+    si.run() # Run the SVD alignment
+    
+    return si
+
+def get_rmsd(fixed_atom_df, moving_atom_df, atom_types=["CA", "N", "C", "O"]):
+    """ Calculate the RMSD between superimposed coordinates of two protein structures.
+    """
+    si = superimpose_structures(fixed_atom_df, moving_atom_df, atom_types=atom_types)
+    return si.get_rms()
+
+def get_lddt(predicted_atom_df, true_atom_df, atom_types=["CA", "N", "C", "O"]):
+    predicted_atom_df = clean_pdb_df(predicted_atom_df, atom_types)
+    true_atom_df = clean_pdb_df(true_atom_df, atom_types)
+
+    coords_predicted = predicted_atom_df[['x_coord', 'y_coord', 'z_coord']].to_numpy()[np.newaxis, :, :]
+    coords_true = true_atom_df[['x_coord', 'y_coord', 'z_coord']].to_numpy()[np.newaxis, :, :]
+    true_pos_mask = np.array([[[1]] * np.shape(coords_true)[1]])
+
+    if coords_predicted.shape[1] != coords_true.shape[1]:
+        warnings.warn("The number of atoms in the predicted and true structures are different.", stacklevel=2)
+        return None
+    else:
+        return np.asarray(lddt(coords_predicted, coords_true, true_pos_mask))[0]
+
+def save_aligned_pdb(fname_fixed, fname_moving, fname_tag):
+    """Align two protein structures from pdb files and save aligned structures
+
+    Args:
+        fname_fixed (str): path to PDB file
+        fname_moving (str): path to PDB file
+
+    Returns:
+        fname_aligned: path to aligned PDB file (rotate/translated version of fname_moving)
+    """
+
+    # load structure to transform
+    structure = PDBParser(QUIET=True).get_structure('', fname_moving)
+    df_fixed = PandasPdb().read_pdb(fname_fixed).df['ATOM']
+    df_moving = PandasPdb().read_pdb(fname_moving).df['ATOM']
+
+    # superimpose on experimental structure file 
+    si = superimpose_structures(df_fixed, df_moving)
+    rot, trans = si.get_rotran()
+    for atom in structure.get_atoms():
+        atom.transform(rot.astype("f"), trans.astype("f"))
+
+    # save modified outputs as PDB files
+    fname_aligned = f"{fname_moving.rstrip('.pdb')}_aligned_{fname_tag}.pdb"
+    io = PDBIO()
+    io.set_structure(structure) 
+    io.save(fname_aligned)
+    
+    return None
+
+def lddt(predicted_points,
+         true_points,
+         true_points_mask,
+         cutoff=15.,
+         per_residue=False):
+  """Measure (approximate) lDDT for a batch of coordinates.
+
+  lDDT reference:
+  Mariani, V., Biasini, M., Barbato, A. & Schwede, T. lDDT: A local
+  superposition-free score for comparing protein structures and models using
+  distance difference tests. Bioinformatics 29, 2722–2728 (2013).
+
+  lDDT is a measure of the difference between the true distance matrix and the
+  distance matrix of the predicted points.  The difference is computed only on
+  points closer than cutoff *in the true structure*.
+
+  This function does not compute the exact lDDT value that the original paper
+  describes because it does not include terms for physical feasibility
+  (e.g. bond length violations). Therefore this is only an approximate
+  lDDT score.
+
+  Args:
+    predicted_points: (batch, length, 3) array of predicted 3D points
+    true_points: (batch, length, 3) array of true 3D points
+    true_points_mask: (batch, length, 1) binary-valued float array.  This mask
+      should be 1 for points that exist in the true points.
+    cutoff: Maximum distance for a pair of points to be included
+    per_residue: If true, return score for each residue.  Note that the overall
+      lDDT is not exactly the mean of the per_residue lDDT's because some
+      residues have more contacts than others.
+
+  Returns:
+    An (approximate, see above) lDDT score in the range 0-1.
+  """
+
+  assert len(predicted_points.shape) == 3
+  assert predicted_points.shape[-1] == 3
+  assert true_points_mask.shape[-1] == 1
+  assert len(true_points_mask.shape) == 3
+
+  # Compute true and predicted distance matrices.
+  dmat_true = np.sqrt(1e-10 + np.sum(
+      (true_points[:, :, None] - true_points[:, None, :])**2, axis=-1))
+
+  dmat_predicted = np.sqrt(1e-10 + np.sum(
+      (predicted_points[:, :, None] -
+       predicted_points[:, None, :])**2, axis=-1))
+
+  dists_to_score = (
+      (dmat_true < cutoff).astype(np.float32) * true_points_mask *
+      np.transpose(true_points_mask, [0, 2, 1]) *
+      (1. - np.eye(dmat_true.shape[1]))  # Exclude self-interaction.
+  )
+
+  # Shift unscored distances to be far away.
+  dist_l1 = np.abs(dmat_true - dmat_predicted)
+
+  # True lDDT uses a number of fixed bins.
+  # We ignore the physical plausibility correction to lDDT, though.
+  score = 0.25 * ((dist_l1 < 0.5).astype(np.float32) +
+                  (dist_l1 < 1.0).astype(np.float32) +
+                  (dist_l1 < 2.0).astype(np.float32) +
+                  (dist_l1 < 4.0).astype(np.float32))
+
+  # Normalize over the appropriate axes.
+  reduce_axes = (-1,) if per_residue else (-2, -1)
+  norm = 1. / (1e-10 + np.sum(dists_to_score, axis=reduce_axes))
+  score = norm * (1e-10 + np.sum(dists_to_score * score, axis=reduce_axes))
+
+  return score
