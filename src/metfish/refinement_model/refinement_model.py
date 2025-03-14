41895cd6d5d8b2d38757fbc0c8abc0bc9d5f9f1d
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-03-14 13:04:23 -0700

    add msa refinement model

---
+++ src/metfish/refinement_model/refinement_model.py
@@ -0,0 +1,142 @@
+
+import time
+import torch
+import pytorch_lightning as pl
+
+from openfold.utils.import_weights import import_jax_weights_
+from openfold.utils.tensor_utils import tensor_tree_map
+from openfold.model.model import AlphaFold
+
+from metfish.msa_model.utils.loss import compute_saxs
+
+
+class SAXSLoss(torch.nn.Module):
+    """SAXS """
+    def __init__(self, config):
+        super(SAXSLoss, self).__init__()
+        self.config = config
+    
+    def forward(self, out, batch):
+        all_atom_pred_pos = out["final_atom_positions"]
+        all_atom_mask = batch["all_atom_mask"]
+        all_atom_true_pos = batch["all_atom_positions"]
+        step = self.config.saxs_loss.step
+        dmax = self.config.saxs_loss.dmax
+
+        # calculate predicted and true saxs
+        pred_saxs = compute_saxs(all_atom_pos=all_atom_pred_pos, all_atom_mask=all_atom_mask, step=step, dmax=dmax)
+        true_saxs = compute_saxs(all_atom_pos=all_atom_true_pos, all_atom_mask=all_atom_mask, step=step, dmax=dmax)
+
+        # get L1 loss
+        l1_loss = torch.nn.L1Loss(reduction="sum")
+        loss = l1_loss(pred_saxs, true_saxs)
+
+        return loss
+
+
+class MSARefinementModel(torch.nn.Module):
+    def __init__(self, config, training=True):
+        super(MSARefinementModel, self).__init__()
+        self.config = config
+        self.af_model = AlphaFold(config)
+        self.training = training
+        
+        for param in self.af_model.parameters():
+            param.requires_grad = False
+        
+        # initialize parameters
+        self.w = None
+        self.b = None
+
+    def initialize_parameters(self, msa):
+        self.w = torch.nn.Parameter(torch.ones_like(msa))
+        self.b = torch.nn.Parameter(torch.zeros_like(msa))
+
+    def forward(self, batch):
+        device = batch['aatype'].device
+        self.w = self.w.to(device)
+        self.b = self.b.to(device)
+
+        # refine msa with linear layer
+        # TODO - msa cluster profile here may mean the msa features after embedding... need to modify if so
+        msa_refined = self.w * batch['msa_feat'] + self.b 
+        batch['msa_feat'] = msa_refined
+
+        # run through alphafold
+        outputs = self.af_model(batch)
+
+        return outputs
+
+# define the lightning module for training
+class MSARefinementModelWrapper(pl.LightningModule):
+    def __init__(self, config, training=True, lr_mul=1.0, lr_add=0.05):
+        super().__init__()
+        self.save_hyperparameters()
+        self.config = config
+        self.model = MSARefinementModel(config)
+        self.training = training
+        if training:
+            self.loss = SAXSLoss(config.loss)
+            self.cached_weights = None
+            self.lr_mul = 1.0
+            self.lr_add = 0.05
+
+        # activate manual optimization
+        self.automatic_optimization = False
+
+        self.num_iterations = 100
+        self.last_log_time = time.time()
+
+        # TODO - initialize MSA refinement parameters as part of config file
+        self.model.initialize_parameters(torch.zeros(256))
+
+    def forward(self, batch):
+        return self.model(batch)
+
+    def _log(self, loss, iter=None, train=True):
+        self.log(
+            f"loss_iter{iter}",
+            loss,
+            on_step=False, on_epoch=True, logger=True, sync_dist=True
+        )
+        self.log('dur', time.time() - self.last_log_time, sync_dist=True)
+        self.last_log_time = time.time()
+
+    def training_step(self, batch):
+        self.model.initialize_parameters(batch['msa_feat'])
+        opt = self.optimizers()
+
+        for n in range(self.num_iterations):
+            print(f'Running iteration {n} / {self.num_iterations}')
+            
+            # clear gradients
+            opt.zero_grad()
+
+            # forward pass
+            outputs = self.model(batch)
+
+            # calculate loss
+            batch_no_recycling = tensor_tree_map(lambda t: t[..., -1], batch)  # remove recycling dimension
+            loss = self.loss(outputs, batch_no_recycling) 
+
+            # backwards pass and update weights
+            self.manual_backward(loss, retain_graph=True)
+            opt.step()
+
+            # log the loss
+            self._log(loss.detach(), iter=n)
+
+        return loss
+    
+    def configure_optimizers(self, learning_rate: float = 1e-3, eps: float = 1e-5,):
+        optimizer = torch.optim.Adam([
+            {'params': [self.model.w], 'lr': self.lr_mul},
+            {'params': [self.model.b], 'lr': self.lr_add}
+        ], eps=eps)
+        
+        return optimizer
+        
+    def load_from_jax(self, jax_path):
+        import_jax_weights_(
+                self.model.af_model, jax_path, version='model_3'
+        )
---
+++ src/metfish/refinement_model/train.py
@@ -0,0 +1,144 @@
+
+import argparse
+import torch
+import os
+import pytorch_lightning as pl
+
+from pytorch_lightning.loggers import CSVLogger, WandbLogger
+from torch.utils.data import DataLoader
+
+from metfish.msa_model.config import model_config
+from metfish.msa_model.data.data_modules import MSASAXSDataset
+from metfish.refinement_model.refinement_model import MSARefinementModelWrapper
+
+# gives a speedup on Ampere-class GPUs
+torch.set_float32_matmul_precision("high")
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "data_dir", type=str,
+    help="Directory containing training pdb, saxs, and msa data",
+)
+parser.add_argument(
+    "output_dir", type=str,
+    help='''Directory in which to output checkpoints, logs, etc. Ignored
+            if not on rank 0''',
+)
+parser.add_argument(
+    "--ckpt_path", type=str,
+    help='''Path to a model checkpoint from which to resume training.''',
+)
+parser.add_argument(
+    "--gpus_per_node", type=int, default=1, help='Number of gpus per node (will use all 4 per node on perlmutter).'
+)
+parser.add_argument(
+    "--num_nodes", type=int, default=1, help='Number of nodes to use for training.'
+)
+parser.add_argument(
+    "--batch_size", type=int, default=2, help='Batch size for each training step'
+)
+parser.add_argument(
+    "--seed", type=int, default=1,
+    help="Random seed"
+)
+parser.add_argument(
+    "--use_wandb", action="store_true", default=True,
+    help="Whether to log metrics to Weights & Biases"
+)
+parser.add_argument(
+    "--fast_dev_run", default=False, action='store_true',
+    help="Whether to run a fast dev run of a single batch for testing purposes"
+)
+parser.add_argument(
+    "--jax_param_path", type=str, default="/pscratch/sd/s/smprince/projects/alphaflow/params_model_1.npz",  # these are the original AF weights,
+    help="""Path to an .npz JAX parameter file with which to initialize the model"""
+)
+parser.add_argument(
+    "--precision", type=str, default='bf16-mixed',
+    help='Sets precision, lower precision improves runtime performance.',
+)
+parser.add_argument(
+    "--max_epochs", type=int, default=100,
+)
+parser.add_argument(
+    "--log_every_n_steps", type=int, default=25,
+)
+parser.add_argument(
+    "--job_name", type=str,
+    help='''Name of job to be used for logging purposes.''',
+)
+
+def main(data_dir="/global/cfs/cdirs/m3513/metfish/PDB70_verB_fixed_data/result",
+         output_dir="/pscratch/sd/s/smprince/projects/metfish/model_outputs",
+         ckpt_path=None,
+         gpus_per_node=1,
+         num_nodes=1,
+         batch_size=2,
+         seed=1,
+         use_wandb=True,
+         fast_dev_run=False,
+         jax_param_path="/pscratch/sd/s/smprince/projects/alphaflow/params_model_1.npz",
+         resume_from_ckpt=False,
+         precision='bf16-mixed',
+         max_epochs=100,
+         log_every_n_steps=25,
+         job_name='default',
+        ):
+    
+    # set up data paths and configuration
+    pdb_dir = f"{data_dir}/pdb"
+    saxs_dir = f"{data_dir}/saxs_r"
+    msa_dir = f"{data_dir}/msa"
+    csv_dir = f"{data_dir}/scripts"
+    training_csv = f'{csv_dir}/input_training.csv'  # NOTE - this was msa_dir for training v_1
+
+    pl.seed_everything(seed, workers=True) 
+    strategy = "ddp" if (gpus_per_node > 1) or num_nodes > 1 else "auto"
+    config = model_config('initial_training', train=True, low_prec=True) 
+    data_config = config.data
+    data_config.common.use_templates = False
+    data_config.common.max_recycling_iters = 0
+
+    # set up training and test datasets and dataloaders
+    train_dataset = MSASAXSDataset(data_config, training_csv, msa_dir=msa_dir, saxs_dir=saxs_dir, pdb_dir=pdb_dir, saxs_ext='.pr.csv', pdb_prefix='')
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
+
+    # initialize model
+    refinement_model = MSARefinementModelWrapper(config)
+
+    # add logging
+    loggers = [CSVLogger(f"{output_dir}/lightning_logs", name="msasaxs")]
+    if use_wandb:
+        os.environ["WANDB__SERVICE_WAIT"] = "300"
+        os.environ["WANDB_MODE"] = "offline"
+        # wandb.init()
+        loggers.append(WandbLogger(name="msasaxs", save_dir=f"{output_dir}/lightning_logs")) 
+
+    # initialize trainer
+    trainer = pl.Trainer(accelerator="gpu", 
+                         strategy=strategy,
+                         max_epochs=max_epochs, 
+                         limit_train_batches=1.0, 
+                         logger=loggers,
+                         log_every_n_steps=log_every_n_steps,
+                         default_root_dir=output_dir,
+                         devices=gpus_per_node,
+                         num_nodes=num_nodes,
+                         fast_dev_run=fast_dev_run, 
+                         precision=precision,
+                        )
+
+    # load existing weights
+    if jax_param_path and not resume_from_ckpt:
+        refinement_model.load_from_jax(jax_param_path)
+        print(f"Successfully loaded JAX parameters at {jax_param_path}...")
+
+    # fit the model
+    trainer.fit(model=refinement_model, train_dataloaders=train_loader, ckpt_path=ckpt_path)
+
+    print('done')
+
+if __name__ == "__main__":
+    main(data_dir="/global/cfs/cdirs/m3513/metfish/NMR_training/data_for_training",
+         output_dir="/pscratch/sd/s/smprince/projects/metfish/model_outputs/",
+         fast_dev_run=True,)
