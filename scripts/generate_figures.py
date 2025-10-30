4d1600badf9bab2b5e3c54bb7f4c1e84caf40c69
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-10-30 12:18:41 -0700

    add argparser to figure generation script - wip

--- scripts/generate_figures.py
+++ scripts/generate_figures.py
@@ -1,33 +1,60 @@
-from pathlib import Path
+import argparse
 
-from metfish.analysis.model_config import ModelConfig
-from metfish.analysis.processor import ModelComparisonProcessor, ProteinStructureAnalyzer
+from pathlib import Path
+from typing import Tuple
+
+from metfish.msa_model.predict import inference
+from metfish.analysis.processor import ModelComparisonProcessor
 from metfish.analysis.visualizer import ProteinVisualization
 
-def main():
- # Setup configuration
+def main( 
+    data_dir: Path,
+    ckpt_dir: Path,
+    output_dir: Path,
+    overwrite: bool = False,
+    models: Tuple[str] = ('AlphaFold', 'SFold_NMR', 'SFold_NMA')
+):
+     # Setup configuration
     model_dict = {
         'AlphaFold': {
             'model_name': 'AlphaFold',
             'tags': 'AF',
-            'pdb_ext': '_atom_only.pdb'
+            'ckpt_path': ckpt_dir / 'af' / 'checkpoints' / 'params_model_1.npz'
         },
-        'AFSAXS_NMRtrain': {
-            'model_name': 'AFSAXS',
-            'tags': 'NMRtrain',
-            'pdb_ext': '_atom_only.pdb'
+        'SFold_NMR': {
+            'model_name': 'SFold',
+            'tags': 'NMR',
+            'ckpt_path': ckpt_dir / 'nmr' / 'checkpoints' / 'epoch=17-step=24282.ckpt',
+        },
+        'SFold_NMA': {
+            'model_name': 'SFold',
+            'tags': 'NMA',
+            'ckpt_path': ckpt_dir / 'nma'/ 'checkpoints' / 'epoch=16-step=17595.ckpt'
         }
     }
     
-    config = ModelConfig(model_dict)
-    analyzer = ProteinStructureAnalyzer(config)
-    
+    # TODO - replace old results archive with the new ones generated from that subset of 40 structures
+    # Generate models
+    for model_key, model_kwargs in model_dict.items():
+        if model_key in models:
+            print(f'Running inference for {model_key}...')
+            inference(**model_kwargs, 
+                    data_dir=data_dir,
+                    output_dir=output_dir / model_kwargs['tags'],
+                    test_csv_name=data_dir / 'input_all.csv',
+                    overwrite=overwrite,
+                    deterministic=True,
+                    random_seed=1234,
+                    pdb_ext='.pdb',
+                    saxs_ext='_atom_only.csv',
+                    save_output_dict=False)
+
+
     # Process comparisons
     processor = ModelComparisonProcessor(
-        model_config=config,
-        analyzer=analyzer,
-        data_dir=Path("/path/to/data"),
-        output_dir=Path("/path/to/output")
+        model_dict=model_dict,
+        data_dir=data_dir,
+        output_dir=output_dir
     )
     
     comparison_df = processor.create_comparison_df(
@@ -37,12 +64,27 @@ def main():
     )
     
     # Visualize results
-    color_scheme = {"AF": "#2b2b2b", "SFold NMR": "#264882", "Target": "#5c5c5c"}
-    label_dict = {"out_AF": "AF", "out_NMRtrain": "SFold NMR", "target": "Target"}
+    color_scheme = {"AF": "#56994A", "Target": "#5c5c5c", "SFold NMR": "#264882", "SFold NMA": "#b13c6c"}
+    label_dict = {"out_AF": "AF", "out_NMR": "SFold NMR", "out_NMA": "SFold NMA", "target": "Target"}
     
-    viz = ProteinVisualization(color_scheme, label_dict)
-    viz.plot_overall_metrics(comparison_df, ['AF', 'NMRtrain'], ['rmsd', 'lddt'])
+    viz = ProteinVisualization(comparison_df, color_scheme, label_dict)
+    viz.plot_overall_metrics(comparison_df, ['AF', 'NMRtrain', 'NMAtrain'], ['rmsd', 'lddt'])
 
 
 if __name__ == "__main__":
-   main()
+    parser = argparse.ArgumentParser(description='Run protein structure analysis')
+    parser.add_argument('--data-dir', type=str, default=Path("/global/cfs/cdirs/m4704/100125_Nature_Com_data/Apo_holo_data"), help='Data directory')
+    parser.add_argument('--ckpt-dir', type=str, default=Path("/global/cfs/cdirs/m4704/100125_Nature_Com_data/single_conformation"), help='Checkpoint directory')
+    parser.add_argument('--output-dir', type=str, default=Path("/global/cfs/cdirs/m4704/100125_Nature_Com_data/results"), help='Output directory')
+    parser.add_argument('--overwrite', action='store_true', default=False, help='Overwrite existing files')
+    parser.add_argument('--models', type=str, nargs='+', default=['AlphaFold', 'SFold_NMR', 'SFold_NMA'], help='Models to run')
+
+    args = parser.parse_args()
+    
+    main(
+        data_dir=Path(args.data_dir),
+        ckpt_dir=Path(args.ckpt_dir),
+        output_dir=Path(args.output_dir),
+        overwrite=args.overwrite,
+        models=args.models
+    )
